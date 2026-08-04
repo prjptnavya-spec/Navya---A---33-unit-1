82aadfb9f9455ce9c13ff082b654fd840fb0fbca
prjptnavya-spec <prjptnavya@gmail.com>
2026-08-04 16:00:14 +0530

    Add files via upload

---
+++ y.py
@@ -0,0 +1,17 @@
+# Program to print a Bio Data using input()
+
+# Taking user inputs
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+gender = input("Enter your gender: ")
+qualification = input("Enter your qualification: ")
+hobby = input("Enter your hobby: ")
+
+# Printing Bio Data
+print("\n------ BIO DATA ------")
+print("Name: ", name)
+print("Age: ", age)
+print("Gender: ", gender)
+print("Qualification: ", qualification)
+print("Hobby: ", hobby)
+print("----------------------")
