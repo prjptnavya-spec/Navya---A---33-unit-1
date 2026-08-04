b341e1fb610dea43192ebe97687bce974b3cb684
prjptnavya-spec <prjptnavya@gmail.com>
2026-08-04 16:10:26 +0530

    Add files via upload

---
+++ i.py
@@ -0,0 +1,7 @@
+a = input("Enter first input: ")
+b = input("Enter second input: ")
+c = input("Enter third input: ")
+
+print("Type of first input:", type(a))
+print("Type of second input:", type(b))
+print("Type of third input:", type(c))
