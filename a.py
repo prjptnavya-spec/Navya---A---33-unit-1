a79ed2afa94df6cf81e6cfb75b987a1376947847
prjptnavya-spec <prjptnavya@gmail.com>
2026-08-04 15:40:29 +0530

    Add files via upload

---
+++ a.py
@@ -0,0 +1,7 @@
+# Program to get user input and print it
+
+# Ask the user to enter something
+user_input = input("Enter something: ")
+
+# Print what the user entered
+print("You entered:", user_input)
